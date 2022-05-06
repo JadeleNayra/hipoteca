ef12cb18a6612910f2fc0ca54aabb32f5ed17e1f
Ana <ana@curso.com>
2022-05-06 17:00:45 +0200

    Primer commit-comienzo con servidor escuchando

---
+++ casas.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+//Inicializo los modulos que instalé con npm
+
+//1.Express
+
+const express=require("express");
+const app=express();
+app.use(express.static("public"));
+
+//2.Body-parser
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Rutas
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+
+
+
+
+//Pongo a escuchar al servidor
+app.listen(3000,function(){
+  console.log("Te estoy escuchando...");
+});
